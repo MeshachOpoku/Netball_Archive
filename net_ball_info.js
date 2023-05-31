432a5eebaa4bd76c8e916b7e51bfacae16350cac
MeshachOpoku <104779012+MeshachOpoku@users.noreply.github.com>
2023-05-30 14:28:15 -1200

    Netball_Archive Commit
    Records players and their achievement in netball competition

---
+++ net_ball_info.js
@@ -0,0 +1,16 @@
+var title;
+var subinfo;
+
+
+title=document.getElementById("title");
+title=document.write("<br><br><br>Award Winning Netball Players  2022 :<hr>");
+subinfo="Empty head! Netball had hidden itself and  I later discovered it when situation became critical. Found book online to read . Qualified! I  became one."
++ "I  got opportunity to train junior high school girls at  a school where I  taught. We started training . Challenges dealing with each other behaviour . \"The need was to connect them together as team.\" Coach thought"+
+".Another one is to sort out good player since Netball Competition was head of us."+
+"They[girls] developed quickly and got used to the game. Magic . No .It was the Lord behind it . "+
+"We made trained girls play with males to see if they can beat them in the game. Starting was bad ,later these girls scored boys  more  goals. Divison came in the boys signifying they loss the game to the trained girls.  "+
+"Lesson  learnt.These girls excelled in  Netball Competition .Won a cup for school.Joy was around. I wish I be in  that past reality."+"These girls are listed below.";
+document.write(subinfo);
+document.write("<hr>1. Mercy :Goal Attack<br>2.Dorothy :Shooter<br>3.Lawrencia :Wing Attack<br>4.Adelaide:Center<br>5.Lovia :Goal defence<br>6.Prisca :Wing Defence<br>7.Georgina :Goal keeper<br>Coach :Meshach Opoku")
+
+
